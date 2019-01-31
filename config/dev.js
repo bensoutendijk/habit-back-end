9a936f7c6b355d0e508cb5296b696492a5416557
Ben Soutendijk <devaent1316@gmail.com>
2019-01-30 21:31:00 -0600

    untrack secret keys file

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  mongoURI: 'mongodb+srv://habit-dev:ggwwsSMLDemSmx42@cluster0-lpdc5.mongodb.net/test?retryWrites=true',
-  cookieKey: '123123123',
-  jwtHttpOnlyKey: 'secret1',
-  jwtKey: 'secret2',
-  awsAccessKey: 'AKIAJYTLSGVD5B5VL74A',
-  awsSecretAccessKey: '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',
-};
