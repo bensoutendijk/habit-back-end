7e82bf01b9fa1a609eb31fb8846bce2af564864a
Ben Soutendijk <devaent1316@gmail.com>
2019-02-02 14:00:06 -0600

    refactor schema

--- models/Project.js
+++ models/Project.js
@@ -2,6 +2,32 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
+const articleSchema = new Schema({
+  type: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  slug: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+});
+
+const sectionSchema = new Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  children: [articleSchema],
+});
+
 const projectSchema = new Schema({
   name: {
     type: String,
@@ -9,29 +35,7 @@ const projectSchema = new Schema({
     unique: true,
     required: true,
   },
-  sections: [{
-    name: {
-      type: String,
-      unique: true,
-      required: true,
-    },
-    children: [{
-      type: {
-        type: String,
-        required: true,
-      },
-      name: {
-        type: String,
-        unique: true,
-        required: true,
-      },
-      slug: {
-        type: String,
-        unique: true,
-        required: true,
-      },
-    }],
-  }],
+  sections: [sectionSchema],
 });
 
 mongoose.model('Project', projectSchema);
