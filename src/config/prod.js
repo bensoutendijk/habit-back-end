c9a488978ff043ef3e19c0afd392c77f222bf770
Ben Soutendijk <devaent1316@gmail.com>
2019-08-01 22:19:02 -0500

    keys

--- src/config/prod.js
+++ src/config/prod.js
@@ -1,4 +1,8 @@
 module.exports = {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
+  jwtHttpOnlyKey: process.env.JWT_SECRET_HTTP,
+  jwtKey: process.env.JWT_SECRET,
+  awsAccessKey: process.env.AWS_ACCESS_KEY,
+  awsSecretAccessKey: process.env.AWS_SECRET_KEY,
 };
