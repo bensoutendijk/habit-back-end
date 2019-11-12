d72462af36b4bdb29b4bc1e3596bc695ca164614
Ben Soutendijk <devaent1316@gmail.com>
2019-11-11 20:21:54 -0600

    remove routes

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,12 +1,8 @@
 import express from 'express';
 import authRoutes from './auth';
-import projectRoutes from './project/projectRoutes';
-import articleRoutes from './article/articleRoutes';
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
-router.use('/projects', projectRoutes);
-router.use('/articles', articleRoutes);
 
 export default router;
