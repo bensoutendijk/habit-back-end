0dd0741258b53e663f520b24a9b51d8af3991394
Ben Soutendijk <devaent1316@gmail.com>
2019-11-12 18:48:43 -0600

    Linting

--- src/models/LocalUser.js
+++ src/models/LocalUser.js
@@ -5,54 +5,54 @@ import jwt from 'jsonwebtoken';
 import keys from '../config/keys';
 
 const userSchema = new Schema({
-  email: { type: String, index: true },
-  permissions: [{ type: String }],
-  services: [{ type: String }],
-  hash: String,
-  salt: String,
+    email: { type: String, index: true },
+    permissions: [{ type: String }],
+    services: [{ type: String }],
+    hash: String,
+    salt: String,
 }, { timestamps: true });
 
 userSchema.methods.setPassword = function setPassword(password) {
-  this.salt = crypto.randomBytes(16).toString('hex');
-  this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
 };
 
 userSchema.methods.validatePassword = function validatePassword(password) {
-  const hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
-  return this.hash === hash;
+    const hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+    return this.hash === hash;
 };
 
 userSchema.methods.generateHttpOnlyJWT = function generateHttpOnlyJWT() {
-  const today = new Date();
-  const expirationDate = new Date(today);
-  expirationDate.setTime(today.getTime() + 1000 * 60 * 30);
+    const today = new Date();
+    const expirationDate = new Date(today);
+    expirationDate.setTime(today.getTime() + 1000 * 60 * 30);
 
-  return jwt.sign({
-    email: this.email,
-    _id: this._id, // eslint-disable-line no-underscore-dangle
-    exp: Math.floor(expirationDate.getTime() / 1000),
-  }, keys.jwtHttpOnlyKey);
+    return jwt.sign({
+        email: this.email,
+        _id: this._id, // eslint-disable-line no-underscore-dangle
+        exp: Math.floor(expirationDate.getTime() / 1000),
+    }, keys.jwtHttpOnlyKey);
 };
 
 userSchema.methods.generateJWT = function generateJWT() {
-  const today = new Date();
-  const expirationDate = new Date(today);
-  expirationDate.setTime(today.getTime() + 1000 * 60 * 30);
+    const today = new Date();
+    const expirationDate = new Date(today);
+    expirationDate.setTime(today.getTime() + 1000 * 60 * 30);
 
-  return jwt.sign({
-    email: this.email,
-    _id: this._id, // eslint-disable-line no-underscore-dangle
-    exp: Math.floor(expirationDate.getTime() / 1000),
-  }, keys.jwtKey);
+    return jwt.sign({
+        email: this.email,
+        _id: this._id, // eslint-disable-line no-underscore-dangle
+        exp: Math.floor(expirationDate.getTime() / 1000),
+    }, keys.jwtKey);
 };
 
 userSchema.methods.toJSON = function toJSON() {
-  return {
-    _id: this.id, // eslint-disable-line no-underscore-dangle
-    email: this.email,
-    permissions: this.permissions,
-    services: this.services,
-  };
+    return {
+        _id: this.id, // eslint-disable-line no-underscore-dangle
+        email: this.email,
+        permissions: this.permissions,
+        services: this.services,
+    };
 };
 
 mongoose.model('LocalUser', userSchema);
