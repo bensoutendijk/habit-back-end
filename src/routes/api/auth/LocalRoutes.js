e81cf52a1db39cb4b76d7562ce1c64aa6a4a752f
Ben Soutendijk <devaent1316@gmail.com>
2019-11-14 21:07:29 -0600

    RepoRoutes

--- src/routes/api/auth/LocalRoutes.js
+++ src/routes/api/auth/LocalRoutes.js
@@ -8,133 +8,133 @@ const User = mongoose.model('LocalUser');
 
 // POST new user route (optional, everyone has access)
 router.post('/', auth.optional, async (req, res) => {
-  const { body: user } = req;
+    const { body: user } = req;
 
-  if (!user.email) {
-    return res.status(422).json({
-      errors: {
-        email: 'is required',
-      },
-    });
-  }
-
-  if (!user.password) {
-    return res.status(422).json({
-      errors: {
-        password: 'is required',
-      },
-    });
-  }
-
-  if (!user.passwordConfirmation) {
-    return res.status(422).json({
-      errors: {
-        passwordConfirmation: 'is required',
-      },
-    });
-  }
-
-  if (user.password !== user.passwordConfirmation) {
-    return res.status(442).json({
-      errors: {
-        passwordConfirmation: 'does not match',
-      },
-    });
-  }
-
-  const existingUser = await User.findOne({ email: user.email });
-
-  if (!existingUser) {
-    const finalUser = new User(user);
-
-    finalUser.setPassword(user.password);
-    try {
-      await finalUser.save();
-    } catch (err) {
-      return res.json({
-        errors: {
-          user: 'Something went wrong',
-        },
-      });
+    if (!user.email) {
+        return res.status(422).json({
+            errors: {
+                email: 'is required',
+            },
+        });
     }
-    res.cookie('token', `Token ${finalUser.generateHttpOnlyJWT()}`, {
-      expires: new Date(Date.now() + 1000 * 60 * 30),
-      httpOnly: true,
+
+    if (!user.password) {
+        return res.status(422).json({
+            errors: {
+                password: 'is required',
+            },
+        });
+    }
+
+    if (!user.passwordConfirmation) {
+        return res.status(422).json({
+            errors: {
+                passwordConfirmation: 'is required',
+            },
+        });
+    }
+
+    if (user.password !== user.passwordConfirmation) {
+        return res.status(442).json({
+            errors: {
+                passwordConfirmation: 'does not match',
+            },
+        });
+    }
+
+    const existingUser = await User.findOne({ email: user.email });
+
+    if (!existingUser) {
+        const finalUser = new User(user);
+
+        finalUser.setPassword(user.password);
+        try {
+            await finalUser.save();
+        } catch (err) {
+            return res.json({
+                errors: {
+                    user: 'Something went wrong',
+                },
+            });
+        }
+        res.cookie('token', `Token ${finalUser.generateHttpOnlyJWT()}`, {
+            expires: new Date(Date.now() + 1000 * 60 * 30),
+            httpOnly: true,
+        });
+        res.cookie('token2', `Token ${finalUser.generateJWT()}`, {
+            expires: new Date(Date.now() + 1000 * 60 * 30),
+        });
+        return res.json({ user: finalUser.toJSON() });
+    }
+    return res.status(442).json({
+        errors: {
+            email: 'already exists',
+        },
     });
-    res.cookie('token2', `Token ${finalUser.generateJWT()}`, {
-      expires: new Date(Date.now() + 1000 * 60 * 30),
-    });
-    return res.json({ user: finalUser.toJSON() });
-  }
-  return res.status(442).json({
-    errors: {
-      email: 'already exists',
-    },
-  });
 });
 
 // POST login route (optional, everyone has access)
 router.post('/login', auth.optional, (req, res, next) => {
-  const { body: user } = req;
+    const { body: user } = req;
 
-  if (!user.email) {
-    return res.status(422).json({
-      errors: {
-        email: 'is required',
-      },
-    });
-  }
-
-  if (!user.password) {
-    return res.status(422).json({
-      errors: {
-        password: 'is required',
-      },
-    });
-  }
-
-  return passport.authenticate('local', { session: false }, (err, passportUser) => {
-    if (err) {
-      return next(err);
+    if (!user.email) {
+        return res.status(422).json({
+            errors: {
+                email: 'is required',
+            },
+        });
     }
 
-    if (passportUser) {
-      res.cookie('token', `Token ${passportUser.generateHttpOnlyJWT()}`, {
-        expires: new Date(Date.now() + 1000 * 60 * 30),
-        httpOnly: true,
-      });
-      res.cookie('token2', `Token ${passportUser.generateJWT()}`, {
-        expires: new Date(Date.now() + 1000 * 60 * 30),
-      });
-      return res.json({ user: passportUser.toJSON() });
+    if (!user.password) {
+        return res.status(422).json({
+            errors: {
+                password: 'is required',
+            },
+        });
     }
 
-    return res.status(400).json({
-      errors: {
-        authentication: 'failure',
-      },
-    });
-  })(req, res, next);
+    return passport.authenticate('local', { session: false }, (err, passportUser) => {
+        if (err) {
+            return next(err);
+        }
+
+        if (passportUser) {
+            res.cookie('token', `Token ${passportUser.generateHttpOnlyJWT()}`, {
+                expires: new Date(Date.now() + 1000 * 60 * 30),
+                httpOnly: true,
+            });
+            res.cookie('token2', `Token ${passportUser.generateJWT()}`, {
+                expires: new Date(Date.now() + 1000 * 60 * 30),
+            });
+            return res.json({ user: passportUser.toJSON() });
+        }
+
+        return res.status(400).json({
+            errors: {
+                authentication: 'failure',
+            },
+        });
+    })(req, res, next);
 });
 
 // GET current route (required, only authenticated users have access)
 router.get('/current', auth.required, (req, res) => {
-  const { localAuth: { _id } } = req;
+    const { localAuth: { _id } } = req;
 
-  return User.findById(_id)
-    .then((user) => {
-      if (!user) {
-        return res.sendStatus(400);
-      }
-      res.cookie('token', `Token ${user.generateHttpOnlyJWT()}`, {
-        expires: new Date(Date.now() + 1000 * 60 * 30),
-        httpOnly: true,
-      });
-      res.cookie('token2', `Token ${user.generateJWT()}`, {
-        expires: new Date(Date.now() + 1000 * 60 * 30),
-      });
-      return res.json({ user: user.toJSON() });
-    });
+    return User.findById(_id)
+        .then((user) => {
+            if (!user) {
+                return res.sendStatus(400);
+            }
+            res.cookie('token', `Token ${user.generateHttpOnlyJWT()}`, {
+                expires: new Date(Date.now() + 1000 * 60 * 30),
+                httpOnly: true,
+            });
+            res.cookie('token2', `Token ${user.generateJWT()}`, {
+                expires: new Date(Date.now() + 1000 * 60 * 30),
+            });
+            return res.json({ user: user.toJSON() });
+        });
 });
 
 export default router;
--- src/routes/api/services/RepoRoutes.js
+++ src/routes/api/services/RepoRoutes.js
@@ -7,28 +7,13 @@ const router = express.Router();
 router.get('/', auth.required, async (req, res) => {
     const { service } = req;
 
-    let { query: { to: dateTo, from: dateFrom } } = req;
-
-    if (!dateTo) {
-        dateTo = new Date().toISOString();
-    }
-
-    if (!dateFrom) {
-        dateFrom = new Date(1970, 1, 1).toISOString();
-    }
-
     switch (service.provider) {
     case 'github': {
         const reposURI = 'https://api.github.com/user/repos';
 
-        let { data: repos } = await axios.get(reposURI, {
+        const { data: repos } = await axios.get(reposURI, {
             headers: { Authorization: `bearer ${service.tokens.accessToken}` },
         });
-
-        repos = repos.map(repo => ({
-            ...repo,
-            _id: repo.time,
-        }));
 
         return res.send(repos);
     }
