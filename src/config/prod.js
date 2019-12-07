312437b827c0cad57fdf17e595b92720ba5801ed
bensoutendijk <devaent1316@gmail.com>
2019-12-07 13:46:15 -0600

    remove single service route

--- src/config/prod.js
+++ src/config/prod.js
@@ -3,6 +3,4 @@ module.exports = {
   cookieKey: process.env.COOKIE_KEY,
   jwtHttpOnlyKey: process.env.JWT_SECRET_HTTP,
   jwtKey: process.env.JWT_SECRET,
-  awsAccessKey: process.env.AWS_ACCESS_KEY,
-  awsSecretAccessKey: process.env.AWS_SECRET_KEY,
 };
--- src/routes/api/services/index.js
+++ src/routes/api/services/index.js
@@ -27,24 +27,6 @@ router.use('/:provider/:username/repos', auth.required, async (req, res, next) =
     next();
 }, repoRoutes);
 
-router.get('/:provider/:username', auth.required, async (req, res) => {
-    const { localAuth: { _id }, params: { provider, username } } = req;
-
-    const { services } = await LocalUser.findById(_id);
-    const users = await OAuthUser.find({ _id: { $in: services } });
-
-    const data = users.filter(user => (
-        user.provider.toLowerCase() === provider.toLowerCase()
-    && user.user.username.toLowerCase() === username.toLowerCase()
-    ));
-
-    res.send(data.map(user => ({
-        _id: user._id,
-        data: user.user,
-        provider: user.provider,
-    })));
-});
-
 router.get('/', auth.required, async (req, res) => {
     const { localAuth: { _id } } = req;
 
